3a5a4d51061579be3ed449d495273fc5a938af58
Aleksander Vizvary <a.vizvary@hotmail.com>
2025-08-07 21:22:08 +0200

    add render URL

--- notesproject/settings.py
+++ notesproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rzibv760-fvs&krt3^m7n!n4gs+*%gtf356fopr!d14^a6ik@5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['notesapp-k6gl.onrender.com']
 
 
 # Application definition
